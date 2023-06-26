ca84b3c530abb30e1481ad17876dc7d426e811a7
Pedro <pedro357bm@gmail.com>
2023-06-26 18:16:27 -0300

    feat: MVP-0001 | Create tournament page layout (#1)
    * tournament page set up with grid
    
    * fixed: countdown
    
    * feat: sidebar for tournament done
    
    * fix: TuournamentCountDown gap
    
    ---------
    
    Co-authored-by: Herbert Araújo <herbertaraujo.contact@gmail.com>

--- services/hx-web/src/app/(app)/[gameSlug]/tournaments/[id]/page.tsx
+++ services/hx-web/src/app/(app)/[gameSlug]/tournaments/[id]/page.tsx
@@ -2,7 +2,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import classnames from "classnames";
-import { CalendarDaysIcon } from "@heroicons/react/20/solid";
+import {
+  CalendarDaysIcon,
+  CurrencyDollarIcon,
+} from "@heroicons/react/20/solid";
 import Button from "@/components/ui/button/button";
 import TournamentTab from "@/components/tournaments/TournamentTab";
 
@@ -21,115 +24,180 @@ export default function Tournament({ params }: TournamentProps) {
   const joinTournament = () => setJoin(!isJoined);
 
   return (
-    <div className="flex flex-grow">
-      <div className="mr-[460px] min-h-screen w-full">
-        <div className="mb-2 h-[200px] rounded-lg bg-hero bg-cover bg-no-repeat ring-1 ring-slate-700 "></div>
-        <TournamentTab />
+    <div className="grid grid-cols-3 gap-6">
+      <div className="col-span-2 border border-dashed border-green-500">
+        {/* left side wraper */}
+        <div className="flex p-3">right side</div>
       </div>
-      <div className="fixed right-[2rem] flex h-screen w-[400px] flex-grow flex-col">
-        <div className="block">
-          <ol className="relative ml-4 border-l border-gray-200 text-gray-500 dark:border-gray-700 dark:text-gray-400">
-            <li className="mb-7 ml-3">
-              <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-green-200 ring-2 ring-white dark:bg-green-900 dark:ring-green-600">
-                <svg
-                  aria-hidden="true"
-                  className="h-3 w-3 text-green-500 dark:text-green-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </span>
-              <div className="pl-4">
-                <h3 className="text-sm font-medium leading-5">
-                  Registration Open
-                </h3>
-                <p className="text-xs">Step for teams get enrolled</p>
+      <div className="col-span-1">
+        <div className="fixed right-0 top-0 flex h-screen w-[380px] flex-col  justify-between pb-8 pr-8 pt-24">
+          {/* sidebar */}
+          <div className="shadow-hi flex h-full flex-col justify-between  rounded-lg border-slate-700 bg-slate-800 p-8 shadow-highlight-100">
+            {/* sidebar header */}
+            <div className="block">
+              <h2
+                data-docsearch-ignore="true"
+                className="mb-3 text-xl font-bold tracking-tight text-slate-900 dark:text-slate-200"
+              >
+                Tournament Name
+              </h2>
+              <div className="text-xs text-gray-400">Created by</div>
+              <div className="py-6">
+                <ol className="relative ml-4 border-l border-gray-200 text-gray-500 dark:border-gray-700 dark:text-gray-400">
+                  <li className="mb-7 ml-3">
+                    <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-green-200 ring-2 ring-white dark:bg-green-900 dark:ring-green-600">
+                      <svg
+                        aria-hidden="true"
+                        className="h-3 w-3 text-green-500 dark:text-green-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                          clipRule="evenodd"
+                        ></path>
+                      </svg>
+                    </span>
+                    <div className="pl-4">
+                      <h3 className="text-sm font-medium leading-5">
+                        Registration Open
+                      </h3>
+                      <p className="text-xs">Step for teams get enrolled</p>
+                    </div>
+                  </li>
+                  <li className="mb-7 ml-3">
+                    <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-gray-100 ring-2 ring-white dark:bg-gray-700 dark:ring-gray-600">
+                      <svg
+                        aria-hidden="true"
+                        className="h-3 w-3 text-gray-500 dark:text-gray-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M10 2a1 1 0 00-1 1v1a1 1 0 002 0V3a1 1 0 00-1-1zM4 4h3a3 3 0 006 0h3a2 2 0 012 2v9a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2zm2.5 7a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm2.45 4a2.5 2.5 0 10-4.9 0h4.9zM12 9a1 1 0 100 2h3a1 1 0 100-2h-3zm-1 4a1 1 0 011-1h2a1 1 0 110 2h-2a1 1 0 01-1-1z"
+                          clipRule="evenodd"
+                        ></path>
+                      </svg>
+                    </span>
+                    <div className="pl-4">
+                      <h3 className="text-sm font-medium leading-tight">
+                        Results tracking
+                      </h3>
+                      <p className="text-xs">
+                        In this step we track the matches results
+                      </p>
+                    </div>
+                  </li>
+                  <li className="mb-7 ml-3">
+                    <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-gray-100 ring-2 ring-white dark:bg-gray-700 dark:ring-gray-600">
+                      <svg
+                        aria-hidden="true"
+                        className="h-3 w-3 text-gray-500 dark:text-gray-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
+                        <path
+                          fillRule="evenodd"
+                          d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
+                          clipRule="evenodd"
+                        ></path>
+                      </svg>
+                    </span>
+                    <div className="pl-4">
+                      <h3 className="text-sm font-medium leading-tight">
+                        Review
+                      </h3>
+                      <p className="text-xs">Step details here</p>
+                    </div>
+                  </li>
+                  <li className="ml-3">
+                    <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-gray-100 ring-2 ring-white dark:bg-gray-700 dark:ring-gray-600">
+                      <svg
+                        aria-hidden="true"
+                        className="h-3 w-3 text-gray-500 dark:text-gray-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
+                        <path
+                          fillRule="evenodd"
+                          d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm9.707 5.707a1 1 0 00-1.414-1.414L9 12.586l-1.293-1.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                          clipRule="evenodd"
+                        ></path>
+                      </svg>
+                    </span>
+                    <div className="pl-4">
+                      <h3 className="text-sm font-medium leading-tight">
+                        Confirmation
+                      </h3>
+                      <p className="text-xs">Step details here</p>
+                    </div>
+                  </li>
+                </ol>
               </div>
-            </li>
-            <li className="mb-7 ml-3">
-              <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-gray-100 ring-2 ring-white dark:bg-gray-700 dark:ring-gray-600">
-                <svg
-                  aria-hidden="true"
-                  className="h-3 w-3 text-gray-500 dark:text-gray-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 2a1 1 0 00-1 1v1a1 1 0 002 0V3a1 1 0 00-1-1zM4 4h3a3 3 0 006 0h3a2 2 0 012 2v9a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2zm2.5 7a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm2.45 4a2.5 2.5 0 10-4.9 0h4.9zM12 9a1 1 0 100 2h3a1 1 0 100-2h-3zm-1 4a1 1 0 011-1h2a1 1 0 110 2h-2a1 1 0 01-1-1z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </span>
-              <div className="pl-4">
-                <h3 className="text-sm font-medium leading-tight">
-                  Results tracking
-                </h3>
-                <p className="text-xs">
-                  In this step we track the matches results
-                </p>
+            </div>
+
+            {/* sidebar footer */}
+            <div className="block border-t-2 border-slate-700 pt-6">
+              <div className="pb-3">
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="col-span-auto border-r border-dashed border-slate-700">
+                    <h3 className="font-mono mb-2 font-medium  dark:text-slate-200  sm:text-sm">
+                      Prize pool
+                    </h3>
+
+                    <div className="flex items-center">
+                      <pre className="preroportional-nums mb-1 text-xl font-extrabold text-white">
+                        1.500,00
+                      </pre>
+                    </div>
+                    <span className="text-xs">BRL</span>
+                  </div>
+                  <div className="col-span-auto">
+                    <h3 className="mb-2 font-medium dark:text-slate-200  sm:text-sm">
+                      Starts in:
+                    </h3>
+                    <TournamentCountdown date={Date.now() + 4000000 * 40} />
+                  </div>
+                </div>
               </div>
-            </li>
-            <li className="mb-7 ml-3">
-              <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-gray-100 ring-2 ring-white dark:bg-gray-700 dark:ring-gray-600">
-                <svg
-                  aria-hidden="true"
-                  className="h-3 w-3 text-gray-500 dark:text-gray-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
-                  <path
-                    fillRule="evenodd"
-                    d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </span>
-              <div className="pl-4">
-                <h3 className="text-sm font-medium leading-tight">Review</h3>
-                <p className="text-xs">Step details here</p>
+
+              <div className="py-4">
+                <div className="flex flex-col">
+                  <div className="mb-2 flex justify-between">
+                    <span className="text-xs text-green-400">4/16 - Teams</span>
+                    <span className="text-xs font-bold uppercase italic text-green-400">
+                      open
+                    </span>
+                  </div>
+                  <div className="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+                    <div
+                      className={classnames(
+                        "h-2.5 rounded-full bg-green-600",
+                        "w-[70%]"
+                      )}
+                    ></div>
+                  </div>
+                </div>
               </div>
-            </li>
-            <li className="ml-3">
-              <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-gray-100 ring-2 ring-white dark:bg-gray-700 dark:ring-gray-600">
-                <svg
-                  aria-hidden="true"
-                  className="h-3 w-3 text-gray-500 dark:text-gray-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
-                  <path
-                    fillRule="evenodd"
-                    d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm9.707 5.707a1 1 0 00-1.414-1.414L9 12.586l-1.293-1.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </span>
-              <div className="pl-4">
-                <h3 className="text-sm font-medium leading-tight">
-                  Confirmation
-                </h3>
-                <p className="text-xs">Step details here</p>
-              </div>
-            </li>
-          </ol>
+              <Button
+                className="w-full"
+                shape="rounded"
+                onClick={joinTournament}
+                color="info"
+              >
+                {isJoined ? "Leave" : "Join"}
+              </Button>
+            </div>
+          </div>
         </div>
-        <TournamentFooter
-          className="hidden md:block"
-          auctionTime={Date.now() + 4000000 * 10}
-          prizePool={2000}
-        />
       </div>
     </div>
   );
--- services/hx-web/src/components/tournaments/TournamentCountdown.tsx
+++ services/hx-web/src/components/tournaments/TournamentCountdown.tsx
@@ -1,59 +1,59 @@
 "use client";
 import React from "react";
-import cn from "classnames";
+import classnames from "classnames";
 import Countdown, { zeroPad } from "react-countdown";
 
 function CountdownDisplayWrapper({ days, hours, minutes, seconds }: any) {
   return (
     <div
-      className={cn(
-        "xs:text-lg flex items-center text-base font-medium -tracking-wider text-gray-900 dark:text-gray-100 md:text-xl xl:text-xl 2xl:text-2xl"
+      className={classnames(
+        "xs:text-lg flex items-center gap-3 font-medium text-slate-900 dark:text-slate-100 md:gap-2 md:text-xl lg:gap-2.5 xl:gap-1 xl:text-xl 2xl:text-2xl "
       )}
     >
       {!!days && (
-        <div className="3xl:w-20 shrink-0">
-          <span className="lining-nums">{zeroPad(days)}</span>
-          <span>d</span>
+        <div className="3xl:w-16 shrink-0">
+          <pre className="text-xl">{zeroPad(days)}</pre>
+          <span className="md:hidden">d</span>
           <span
-            className={cn(
-              "hidden truncate pt-2.5 text-sm -tracking-wide text-gray-600 dark:text-gray-400 "
+            className={classnames(
+              "hidden truncate pt-2.5 text-xs text-slate-600 dark:text-slate-400 md:block"
             )}
           >
             Days
           </span>
         </div>
       )}
-      <div className="3xl:w-20 shrink-0">
-        <span className="">{zeroPad(hours)}</span>
-        <span>h</span>
+      <div className="3xl:w-16 shrink-0">
+        <pre className="text-xl">{zeroPad(hours)}</pre>
+        <span className="md:hidden">h</span>
         <span
-          className={cn(
-            "hidden truncate pt-2.5 text-sm -tracking-wide text-gray-600 dark:text-gray-400"
+          className={classnames(
+            "truncate pt-2.5 text-xs text-slate-600 dark:text-slate-400 md:block"
           )}
         >
           Hours
         </span>
       </div>
-      <div className="3xl:w-20 shrink-0">
-        <span className="">{zeroPad(minutes)}</span>
-        <span>m</span>
+      <div className="3xl:w-16 shrink-0">
+        <pre className="text-xl">{zeroPad(minutes)}</pre>
+        <span className="md:hidden">m</span>
         <span
-          className={cn(
-            "hidden truncate pt-2.5 text-sm -tracking-wide text-gray-600 dark:text-gray-400 "
+          className={classnames(
+            "truncate pt-2.5 text-xs text-slate-600 dark:text-slate-400 md:block"
           )}
         >
-          Minutes
+          Mins
         </span>
       </div>
-      <div className="3xl:w-20 shrink-0">
-        <span className="">{zeroPad(seconds)}</span>
-        <span>s</span>
+      <div className="3xl:w-16 shrink-0">
+        <pre className="text-xl">{zeroPad(seconds)}</pre>
+        <span className="md:hidden">s</span>
         <span
-          className={cn(
-            "hidden truncate pt-2.5 text-sm -tracking-wide text-gray-600 dark:text-gray-400 "
+          className={classnames(
+            "hidden truncate pt-2.5 text-xs text-slate-600 dark:text-slate-400 md:block"
           )}
         >
-          Seconds
+          Secs
         </span>
       </div>
     </div>

45ce03ef44bc35a9076067e7e7bf529a1d1a6599
Herbert Araújo <herbertaraujo.contact@gmail.com>
2023-06-29 16:19:10 -0300

    fix: tab transition and hide nav scrollbar

--- services/hx-web/tailwind.config.js
+++ services/hx-web/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 const { fontSize } = require("./src/app/theme/fontStyles");
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   content: [
@@ -36,5 +37,20 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        /* Hide scrollbar for Chrome, Safari and Opera */
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none",
+        },
+
+        /* Hide scrollbar for IE, Edge and Firefox */
+        ".no-scrollbar": {
+          "-ms-overflow-style": "none" /* IE and Edge */,
+          "scrollbar-width": "none" /* Firefox */,
+        },
+      });
+    }),
+  ],
 };

57710c1db25021e737711f6c652b2048587e764b
Herbert Araújo <herbertaraujo.contact@gmail.com>
2023-06-29 18:04:58 -0300

    feat: tournament tab rules content

---
+++ services/hx-web/src/components/tournaments/tournament-main/TournamentRules.tsx
@@ -0,0 +1,200 @@
+import classnames from "classnames";
+
+interface TournamentRulesProps {
+  classNames?: string;
+}
+
+interface TournamentTextProps {
+  children: React.ReactNode;
+  className?: string;
+}
+const TournamnetText = ({ children, className }: TournamentTextProps) => (
+  <p className={classnames("", className)}>{children}</p>
+);
+
+export const TournamentRules: React.FC<TournamentRulesProps> = ({
+  classNames
+}) => {
+  return (
+    <main className={classnames("p-6", classNames)}>
+      <h2 className="text-center text-base font-bold tracking-tighter text-slate-400 md:text-xl">
+        Tournament Rules
+      </h2>
+
+      <section className="my-2 flex flex-col gap-2">
+        <p>
+          This competition is not affiliated with or sponsored by Riot Games,
+          Inc. or League of Legends Esports.
+        </p>
+        <p>
+          Refer to the tournament details to see what game modes are tracked.
+        </p>
+        <p>
+          Smurfing will result in an immediate and permanent ban from Repeat.gg
+          and all winnings will be forfeited. This can include, but is not
+          limited to, creating a new account to play with or intentionally
+          losing games to lower your rank or affect your matchmaking lobbies.
+        </p>
+        <p>
+          You must play the minimum number of games in order to get paid out in
+          a tournament. The minimum number of games to play is the same as the
+          number of games we count for your score, which can be found in the
+          Tournament Details.
+        </p>
+        <p>
+          Winners of digital prizes have 14 days after the tournament ends to
+          claim the prize.
+        </p>
+        <p>
+          Winners of physical prizes have 7 days after the tournament ends to
+          claim the prize.
+        </p>
+        <p>
+          If users end up with the same score in the tournament, the tiebreaker
+          for prize pool will prioritize the user who entered the tournament
+          first.
+        </p>
+        <h3>When Physical Products Tournament Are Given to Top ## Places</h3>
+        <p>
+          No Purchase is necessary to enter or win. Odds of winning depend on
+          the number of entries within the tournament. Giveaway winners are
+          subject to all applicable federal, state and local laws and
+          regulations. Void where prohibited. By entering the Tournament
+          Giveaway, the contestant (&ldquot;You&rdquot;) agree to comply with
+          and abide by these Rules and the decisions of Repeat.gg, and you
+          represent and warrant that you meet the eligibility requirements. In
+          addition, you agree to accept Repeat.gg&quot;s decisions as final and
+          binding as it relates to the content of this campaign.
+        </p>
+        <p>
+          How to enter: By joining this tournament you have entered the
+          giveaway.
+        </p>
+        <p>
+          You must provide the information requested by Repeat in order to claim
+          the prize. You may not enter more times than indicated by using
+          multiple email addresses, identities, or devices in an attempt to
+          circumvent the rules. If you use fraudulent methods or otherwise
+          attempt to circumvent the rules of the giveaway, your submission may
+          be disqualified at the sole discretion of Repeat.gg.
+        </p>
+        <p>
+          Actual/appraised value of prizes may differ at time of prize award.
+          The specifics of the prize shall be solely determined by Repeat.gg. No
+          cash or other prize substitution shall be permitted except for at
+          Repeat.gg&quot;s discretion. The prize is non transferable.
+          Substitution of the prize or transfer/assignment of the prize to
+          others or request for the cash equivalent of the prize by the Winner
+          is not permitted. Any and all prize related expenses, including
+          without limitation any and all federal, state, and/or local taxes
+          shall be the sole responsibility of the Winner. Acceptance of the
+          prize constitutes permission for Repeat.gg to use the Winner&quot;s
+          name, likeness, and entry for purposes of advertising and trade
+          without further compensation, unless prohibited by law.
+        </p>
+        <p>
+          Winners will be notified within 7 days of the winner selection.
+          Repeat.gg shall have no liability for a Winner&quot;s failure to
+          receive notices due to spam, junk e-mail or other security settings or
+          for a Winner&quot;s provision of incorrect or otherwise
+          non-functioning contact information. If the Winner cannot be
+          contacted, is ineligible, fails to claim the prize within 7 days from
+          the time award notification was sent, or fails to timely return a
+          completed and executed declaration and release as required, the prize
+          may be forfeited and an alternate Winner selected. Receipt of the
+          prize offered in this Campaign by the Winner is conditioned upon
+          compliance with any and all federal, state, and local laws and
+          regulations. Any violation of these official rules by the Winner at
+          Repeat.gg&quot;s sole discretion will result in the Winner&quot;s
+          disqualification as winner, and all privileges as winner will be
+          immediately terminated.
+        </p>
+        <p>
+          Repeat.gg reserves the right, in its sole discretion, to cancel,
+          terminate, modify or suspend the Tournament Giveaway should virus,
+          bug, non-authorised human intervention, fraud, or other cause beyond
+          Repeat.gg&quot;s control corrupt or affect the administration,
+          security, fairness, or proper conduct of the Tournament Giveaway. In
+          such cases, Repeat.gg may select a winner(s) from eligible entries
+          received before and/or after the action taken by Repeat.gg if
+          appropriate. Repeat.gg reserves the right at its sole discretion to
+          disqualify any individual who tampers or attempts to tamper with the
+          entry process or the operation of the Tournament Giveaway or website
+          or violates these Terms &amp; Conditions. Repeat.gg has the right, in
+          its sole discretion, to maintain the integrity of the Tournament
+          Giveaway, to void entries for any reason, including, but not limited
+          to: multiple entries from the same user from different IP addresses;
+          multiple entries from the same computer in excess of that allowed by
+          Tournament Giveaway rules; or the use of bots, macros, scripts, or
+          other technical means for entering. Any attempt by an entrant to
+          deliberately damage any website or undermine the legitimate operation
+          of the Tournament Giveaway may be a violation of criminal and civil
+          laws. Should such an attempt be made, Repeat.gg reserves the right to
+          seek damages to the fullest extent permitted by law.
+        </p>
+        <p>
+          Limitation of Liability: By entering the Tournament Giveaway you agree
+          to release and hold harmless Repeat.gg and its subsidiaries,
+          affiliates, advertising and promotion agencies, partners,
+          representatives, agents, successors, assigns, employees, officers and
+          directors from any liability, illness, injury, death, loss,
+          litigation, claim or damage that may occur, directly or indirectly,
+          whether caused by negligence or not, from (i) such entrant&quot;s
+          participation in the Tournament Giveaway and/or his/her acceptance,
+          possession, use, or misuse of any prize or any portion thereof; (ii)
+          unauthorized human intervention in any part of the Tournament
+          Giveaway; (iii) electronic or human error in the administration of the
+          Tournament Giveaway or the processing of entries; (iv) technical
+          errors of any kind, including but not limited to the malfunction of
+          any computer, cable, network, hardware, or software, or other
+          mechanical equipment; (v) the unavailability or inaccessibility of any
+          transmissions, telephone, or Internet service; (vi) printing errors;
+          (vii) lost, late, postage due, misdirected, or undeliverable mail.
+        </p>
+        <p>
+          Disputes: This Tournament Giveaway is governed by the laws of United
+          States, without respect to conflict of law doctrines. By participating
+          in this Campaign, you agree that any and all disputes that cannot be
+          resolved between the parties, and causes of action arising out of or
+          connected with this Campaign, shall be resolved individually, without
+          resort to any form of class action, exclusively before a court located
+          in United States having jurisdiction. Further, in any such dispute,
+          under no circumstances shall You be permitted to obtain awards for,
+          and hereby waives all rights to, punitive, incidental, or
+          consequential damages, including reasonable attorney&quot;s fees,
+          other than actual out-of-pocket expenses (i.e. costs associated with
+          entering the Tournament Giveaway). You further waive all rights to
+          have damages multiplied or increased.
+        </p>
+        <p>
+          The Tournament Giveaway is in no way sponsored, endorsed, administered
+          by, or associated with Facebook, Twitter, Instagram, Snapchat,
+          YouTube, Reddit, Pinterest, LinkedIn or any other social networks that
+          are used to share the Tournament Giveaway. You understand that you are
+          providing your information to the owner of the Tournament Giveaway and
+          not to Facebook, Twitter, Instagram, Snapchat, YouTube, Reddit,
+          Pinterest, LinkedIn or any other social networks.
+        </p>
+        <p>
+          Acceptance of Rules: By participating in the tournament giveaway, You
+          have affirmatively reviewed, accepted, and agreed to all of the
+          Official Rules, Terms and Conditions.
+        </p>
+        <h3>When Tournament Includes a Random Giveaway or “Door” Prize </h3>
+        <p>
+          Though this Tournament Giveaway awards winners at random, Repeat.gg
+          are not bound by US lottery rules as there is no consideration of
+          entry to participate in the giveaway.
+        </p>
+        <p>
+          Consideration - of which there are two types of consideration consists
+          of; Monetary and Non-Monetary. Monetary is the purchase of a
+          Sponsor&quot;s product or any other payment/entry fee required to
+          enter. Non-monetary is substantial time or effort expended which
+          benefits the Sponsor in some direct way.
+        </p>
+        <p>*All the above terms and conditions also apply*</p>
+      </section>
+    </main>
+  );
+};
--- services/hx-web/src/components/tournaments/tournament-main/TournamentTab.tsx
+++ services/hx-web/src/components/tournaments/tournament-main/TournamentTab.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Tab } from "@headlessui/react";
+import { TournamentRules } from "./TournamentRules";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -7,93 +8,9 @@ function classNames(...classes: string[]) {
 
 export default function TournamentTab() {
   let [categories] = useState({
-    Overview: [
-      {
-        id: 1,
-        title: "Does drinking coffee make you smarter?",
-        date: "5h ago",
-        commentCount: 5,
-        shareCount: 2
-      },
-      {
-        id: 2,
-        title: "So you've bought coffee... now what?",
-        date: "2h ago",
-        commentCount: 3,
-        shareCount: 2
-      }
-    ],
-    Teams: [
-      {
-        id: 1,
-        title: "Is tech making coffee better or worse?",
-        date: "Jan 7",
-        commentCount: 29,
-        shareCount: 16
-      },
-      {
-        id: 2,
-        title: "The most innovative things happening in coffee",
-        date: "Mar 19",
-        commentCount: 24,
-        shareCount: 12
-      }
-    ],
-    Brackets: [
-      {
-        id: 1,
-        title: "Ask Me Anything: 10 answers to your questions about coffee",
-        date: "2d ago",
-        commentCount: 9,
-        shareCount: 5
-      },
-      {
-        id: 2,
-        title: "The worst advice we've ever heard about coffee",
-        date: "4d ago",
-        commentCount: 1,
-        shareCount: 2
-      }
-    ],
-    Rules: [
-      {
-        id: 1,
-        title: "Ask Me Anything: 10 answers to your questions about coffee",
-        date: "2d ago",
-        commentCount: 9,
-        shareCount: 5
-      },
-      {
-        id: 2,
-        title: "The worst advice we've ever heard about coffee",
-        date: "4d ago",
-        commentCount: 1,
-        shareCount: 2
-      }
-    ],
-    Support: [
-      {
-        id: 1,
-        title: "Ask Me Anything: 10 answers to your questions about coffee",
-        date: "2d ago",
-        commentCount: 9,
-        shareCount: 5
-      },
-      {
-        id: 2,
-        title: "The worst advice we've ever heard about coffee",
-        date: "4d ago",
-        commentCount: 1,
-        shareCount: 2
-      },
-      {
-        id: 3,
-        title: "The worst advice we've ever heard about coffee",
-        date: "4d ago",
-        commentCount: 1,
-        shareCount: 2
-      }
-    ]
+    Overview: <div>Teams</div>,
+    Teams: <div>Teams</div>,
+    Rules: <TournamentRules />
   });
 
   return (
@@ -116,40 +33,13 @@ export default function TournamentTab() {
             </Tab>
           ))}
         </Tab.List>
-        <Tab.Panels className="mt-2">
-          {Object.values(categories).map((posts, idx) => (
+        <Tab.Panels className="mx-6">
+          {Object.values(categories).map((component, idx) => (
             <Tab.Panel
               key={idx}
-              className={classNames("rounded-xl bg-slate-800 p-3")}
+              className={classNames("rounded-xl bg-slate-800")}
             >
-              <ul className="flex flex-col gap-3">
-                {posts.map((post) => (
-                  <li
-                    key={post.id}
-                    className="relative rounded-md p-3 hover:bg-slate-700"
-                  >
-                    <h3 className="text-sm font-medium leading-5">
-                      {post.title}
-                    </h3>
-
-                    <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-slate-500">
-                      <li>{post.date}</li>
-                      <li>&middot;</li>
-                      <li>{post.commentCount} comments</li>
-                      <li>&middot;</li>
-                      <li>{post.shareCount} shares</li>
-                    </ul>
-
-                    <a
-                      href="#"
-                      className={classNames(
-                        "absolute inset-0 rounded-md",
-                        "ring-slate-400 focus:z-10 focus:outline-none focus:ring-2"
-                      )}
-                    />
-                  </li>
-                ))}
-              </ul>
+              {component}
             </Tab.Panel>
           ))}
         </Tab.Panels>

7d515509f2c2286d14711ae39c75e6e88ad5f767
Herbert Araújo <herbertaraujo.contact@gmail.com>
2023-06-30 15:11:14 -0300

    feat: add TournamentTeams layout

--- services/hx-web/tailwind.config.js
+++ services/hx-web/tailwind.config.js
@@ -50,6 +50,13 @@ module.exports = {
           "-ms-overflow-style": "none" /* IE and Edge */,
           "scrollbar-width": "none" /* Firefox */,
         },
+
+        dropShadow: {
+          glow: [
+            "0 0px 20px rgba(255,255, 255, 0.35)",
+            "0 0px 65px rgba(255, 255,255, 0.2)",
+          ],
+        },
       });
     }),
   ],
